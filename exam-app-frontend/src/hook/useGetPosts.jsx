411f387b1c18054fb35343c5f5cbc8f146ecdb8d
Mikkel Hansen <Mikkelhansen077@gmail.com>
2025-06-17 18:12:18 +0200

    added UI feature

---
+++ exam-app-frontend/src/hook/useGetPosts.jsx
@@ -0,0 +1,26 @@
+// src/api/posts.js
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+
+export default function useGetPosts() {
+
+    const BASE_URL = "http://localhost:8080/post";
+
+    const getPosts = async () => {
+        const response = await axios.get(BASE_URL);
+        return response.data;
+    }
+
+    const {data: posts = [], error, isLoading} = useQuery({
+        queryFn: getPosts,
+        queryKey: "posts",
+    })
+
+    return (
+        {
+            posts,
+            error,
+            isLoading,
+        }
+    )
+}
